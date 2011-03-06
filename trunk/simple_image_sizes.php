ca72c49212bf675f72af9f93d0307494cda2cd87
Rahe <Rahe@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-06 10:26:56 +0000

    1.0.2    - License fix
    git-svn-id: http://svn.wp-plugins.org/simple-image-sizes@356103 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/simple_image_sizes.php
+++ trunk/simple_image_sizes.php
@@ -3,17 +3,28 @@
 Plugin Name: Simple Image Size
 Plugin URI: http://wordpress.org/extend/plugins/simple-image-sizes/
 Description: Add an option in media setting page for images sizes
-Version: 1.0.1
+Version: 1.0.2
 Author: Rahe
 Author URI: http://www.beapi.fr
 Text Domain: sis
 Domain Path: /languages/
 Network: false
 
-Copyright 2010 Nicolas JUEN (njuen@beapi.fr) - Be-API
+Copyright 2011 Nicolas JUEN (njuen@beapi.fr) - Be-API
 
-This plugin is not free to usage, not open-source, not GPL.
-You can't use and modify this plugin without the permission of Be-API. (njuen@beapi.fr)
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
 define( 'SIS_URL', plugins_url('/', __FILE__) );

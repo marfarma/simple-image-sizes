df1dc70b1d0edfb9dbfe24ede2606c98726893de
Rahe <Rahe@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-31 00:04:05 +0000

    * 2.3.1 	* Add Ajax bulk actions on medias list 	* Add ajax thumbnail rebuild on single media
    git-svn-id: http://svn.wp-plugins.org/simple-image-sizes/trunk@525500 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/sis-attachments.js
+++ js/sis-attachments.js
@@ -1,6 +1,37 @@
-jQuery( function(){
+jQuery( function() {
+	// Add action dinamycally
+	jQuery( 'select[name="action"]' ).append( 
+		jQuery( '<option/>' ).attr( 'value', 'sis-regenerate' ).text( sis.regenerate )
+	);
+	
+	// Regenerate one element
 	jQuery( '.sis-regenerate-one' ).click( function( e ) {
 		e.preventDefault();
+		new SISAttachRegenerate( this );
+	});
+	
+	// On bulk actions
+	jQuery( '#doaction' ).click( function( e ) {
+		if( jQuery( this ).parent().find( 'select' ).val() == 'sis-regenerate' ) {
+			// Get checked checkbocxes
+			var els = jQuery( '.wp-list-table.media #the-list tr input[type="checkbox"]:checked' );
+			
+			// Check there is any elements selected
+			if( els.length > 0 ) {
+				
+				// Stop default action
+				e.preventDefault();
+				
+				// Make all the selected elements
+				els.each( function( i,el ) {
+					new SISAttachRegenerate( jQuery( this ) );
+				} )
+			}
+		}
+	} );
+	
+	// Function for regenerating the elements
+	var SISAttachRegenerate = function( el ) {
 		var regenerate = {
 			list : '',
 			percent : '' ,
@@ -9,13 +40,21 @@ jQuery( function(){
 			messageZone : '',
 			init: function( el ) {
 				this.el = jQuery( el );
-				id = jQuery( el ).attr( 'id' );
-				this.id = id.replace( 'post-', '' );
+				id = this.el.attr( 'id' );
+				
+				// IF no id found
+				if( !id ) {
+					id = this.el.closest( '.media-item' ).attr( 'id' );
+					this.id = id.replace( 'media-item-', '' );
+				} else {
+					this.id = id.replace( 'post-', '' );
+				}
 				
 				this.list = { 'id' : this.id, 'title' : 'titre' };
 				
 				if( this.el.find('.title em').size() == 0 )
 					this.el.find('.title strong').after('<em/>');
+				
 				this.messageZone = this.el.find('.title em');
 				
 				if( !this.el.hasClass( 'ajaxing' ) )
@@ -52,9 +91,10 @@ jQuery( function(){
 						_self.el.fadeTo( 'fast' ,'1' ).removeClass('ajaxing');
 					}
 				});
-		
 			}
 		}
-		regenerate.init( jQuery( this ).closest( 'tr' ) );
-	});
-});
+		
+		// Launch regeneration
+		regenerate.init( jQuery( el ).closest( 'tr' ) );
+	}
+} );
